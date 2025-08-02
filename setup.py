c88e85e924aff333617b69193adfb35a109ed3ca
Eugene Scherba <escherba@gmail.com>
2025-08-02 13:37:13 -0700

    v0.4.0 (#33)
    * ci: bump actions/upload-artifact to v4
    * ci: bump ubuntu os to 24.04
    * add cp312 support (v0.3.4)
    * ci: bump cibuildwheel to 2.23.3
    * Modernize to build with newer Python 3 versions; improve tests (#32)
    
    Using the int cast in the 128-bit logic causes Cython to emit
    __Pyx_PyLong_From_uint64_t((__pyx_v_result.first << 64ULL))
    instead of the broken
    PyNumber_Lshift(__pyx_t_9, __pyx_mstate_global->__pyx_int_64L)
    when we shift left a "long" int with 64ULL
    
    Added a test that actually checks the value returned for the
    128-bit hashes.
    
    Co-authored-by: Eugene Scherba <escherba@gmail.com>
    
    * add cp313 and update workflows
    
    ---------
    
    Co-authored-by: florianvazelle <ponythugflorian@gmail.com>
    Co-authored-by: Martin Falatic <martin@falatic.com>

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ EXT_MODULES = [
     ),
 ]
 
-VERSION = "0.3.3"
+VERSION = "0.4.0"
 URL = "https://github.com/escherba/python-metrohash"
 
 
@@ -162,6 +162,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries",
         "Topic :: System :: Distributed Computing",
